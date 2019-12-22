4c93022c648725a226c71acb0ef8f399b2ac8fbc
Felipe Zipitria <fzipitria@perceptyx.com>
2019-12-22 16:56:18 -0300

    Use received args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/sh -l
 
 echo "--- Building SAMM Web markdown"
-echo "*** Datafiles dir: /github/workspace/Datafiles"
-echo "*** Destination dir: /github/workspace/output"
 
-ls -l /github/*
-
-touch /github/home/TEST1 /github/workflow/TEST1 /github/workspace/TEST1
-touch /github/home/TEST1 
-touch /github/workflow/TEST1
-touch /github/workspace/TEST1
-
-/build/make_web.sh -d /github/workspace/Datafiles -o /github/workspace/output
+/build/make_web.sh $*
