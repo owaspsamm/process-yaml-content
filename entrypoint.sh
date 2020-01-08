ed223686d8cfb04c769a3993f039fe7801c92859
Felipe Zipitria <fzipitria@perceptyx.com>
2020-01-08 09:17:14 -0300

    Run shellcheck. Use parameter datafiles.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ export PATH=$PATH:/build:/build/bin
 
 echo "--- Building SAMM Web markdown"
 
-/build/make_web.sh $*
+/build/make_web.sh "$*"
--- samm2yaml2md/make_web.sh
+++ samm2yaml2md/make_web.sh
@@ -15,19 +15,19 @@
 # USAGE STATEMENT
 function usage() {
 cat << EOF
-usage: $0 -d Datafiles_dir -o output_dir
+usage: "$0" -d Datafiles_dir -o output_dir
 
 EOF
 }
 
 
 while getopts "d:o:" OPTION; do
-  case $OPTION in
+  case "$OPTION" in
     d)
-      DATAFILES=$OPTARG
+      DATAFILES="$OPTARG"
       ;;
     o)
-      OUTPUT=$OPTARG
+      OUTPUT="$OPTARG"
       ;;
     ?)
       echo "ERROR: Invalid Option $OPTION Provided!"
@@ -38,7 +38,7 @@ while getopts "d:o:" OPTION; do
   esac
 done
   
-BASEDIR=$(dirname $0)
+BASEDIR=$(dirname "$0")
 
 src=$BASEDIR/web
 
@@ -48,26 +48,26 @@ src=$BASEDIR/web
 #
 
 echo tidying up any leftover files
-$BASEDIR/bin/cleanup_env_for_make_web.sh $OUTPUT
+"$BASEDIR"/bin/cleanup_env_for_make_web.sh "$OUTPUT"
 
-test -e $src/static.templates && echo "writing ns files to namespaces" && cp $src/static.templates/*.template $OUTPUT/templates/
+test -e "$src/static.templates" && echo "writing ns files to namespaces" && cp "$src/static.templates/*.template" "$OUTPUT/templates/"
 
-$BASEDIR/bin/make_namespaces.py --target web --output $OUTPUT --yaml FIXME Datafiles/*.yml 
+"$BASEDIR"/bin/make_namespaces.py --target web --output "$OUTPUT" --yaml "$DATAFILES"/*.yml 
 
 echo "mapping namespaces to templates"
-$BASEDIR/bin/map_web_ns2template.sh $OUTPUT
+"$BASEDIR"/bin/map_web_ns2template.sh "$OUTPUT"
 
 echo creating ./run_make_markdown_script.sh
 # generate the script that automates the calls to "make_markdown.py namespaces/foo.ns templates/generic.template"
 
-$BASEDIR/create_make_web_markdown_script.sh $OUTPUT $BASEDIR/run_make_web_markdown_script.sh
+"$BASEDIR"/create_make_web_markdown_script.sh "$OUTPUT" "$BASEDIR"/run_make_web_markdown_script.sh
 test $? -ne 0 && echo "create_make_web_markdown_script.sh failed" && exit
 
 echo runing ./run_make_web_markdown_script.sh
 # this script is created by ./create_make_markdown_script.sh
-$BASEDIR/run_make_web_markdown_script.sh
+"$BASEDIR"/run_make_web_markdown_script.sh
 
 echo "fixing up markdown files.."
-$BASEDIR/bin/fixup_web_markdown.sh $OUTPUT/markdown/
+"$BASEDIR"/bin/fixup_web_markdown.sh "$OUTPUT"/markdown/
 
 echo "done"
