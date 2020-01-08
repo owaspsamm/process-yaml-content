10de7557eaf5df7fa4f57615242f37aecb9cc5d3
Felipe Zipitria <fzipitria@perceptyx.com>
2020-01-08 14:16:01 -0300

    Prevent work splitting

--- samm2yaml2md/bin/cleanup_env_for_make_web.sh
+++ samm2yaml2md/bin/cleanup_env_for_make_web.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-test -z $1 && echo "usage: $0 <base dir to cleanup>" && exit 1
+test -z "$1" && echo "usage: $0 <base dir to cleanup>" && exit 1
 
-web=$1
+web="$1"
 
 # these directories should be completely empty actually.
 # we're just careful for now in case a precious artifact is left over.
 # adjust when we have a stable working version
-rm -f $web/namespaces/*.ns
-rm -f $web/templates/*.template
-rm -f $web/ns2template.mapping/*.ns
-rm -f $web/markdown/*.md
+rm -f "$web/namespaces/*.ns"
+rm -f "$web/templates/*.template"
+rm -f "$web/ns2template.mapping/*.ns"
+rm -f "$web/markdown/*.md"
 
-mkdir -p $web/{namespaces,templates,ns2template.mapping,markdown}
+mkdir -p "$web/{namespaces,templates,ns2template.mapping,markdown}"
